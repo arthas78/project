173411cf10eafd1496a7a79c87b74ce15ea98721
Ihor <ihorbolzhelarskyi78@gmail.com>
2024-11-27 19:26:24 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+"use strict";
+
+console.log(`arr` + "-object");
+console.log(4 + +"5");
+
+let incr = 10,
+  decr = 10;
+
+incr++;
+decr--;
+console.log(incr);
+consle.log(decr);
+
+const isChecked = true;
+
+const isClosed = false;
+
+console.logg(isClosed && isChecked);

17eb273330cfb96ffc5012dace46190c5b22a7b8
Ihor <ihorbolzhelarskyi78@gmail.com>
2024-11-28 20:17:04 +0000

    First commit

--- script1.js
+++ script1.js
@@ -1,1 +1,8 @@
-let minion = "minionchik";
+const numberOfFilms = prompt(`How many movies have you watch already?""`);
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};

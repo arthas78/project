746e693e849777fcff5daa6cecd37e6e3e0b7425
Ihor <ihorbolzhelarskyi78@gmail.com>
2024-11-28 20:54:40 +0000

    last commit

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-"use strict";
+const numberOfFilms = +prompt(`How many movies have you watch already?`, ``);
+let personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  priv: false,
+};
 
-console.log(`arr` + "-object");
-console.log(4 + +"5");
+const a = prompt(`What was the last movie you watched?`, ``);
+const b = prompt(`How much would you rate it?`, ``);
+const c = prompt(`What was the last movie you watched?`, ``);
+const d = prompt(`How much would you rate it?`, ``);
 
-let incr = 10,
-  decr = 10;
-
-incr++;
-decr--;
-console.log(incr);
-console.log(decr);
-
-const isChecked = true;
-
-const isClosed = false;
-
-console.log(isClosed && isChecked);
-const lol = 13;
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+console.log(personalMovieDB);
--- script1.js
+++ script1.js
@@ -1,8 +1,18 @@
-const numberOfFilms = prompt(`How many movies have you watch already?""`);
+const numberOfFilms = prompt(`How many movies have you watch already?,""`);
 let personalMovieDB = {
   count: numberOfFilms,
-  movies: {},
+  movies: {
+    lastMovie: rate,
+  },
   actors: {},
   genres: [],
   privat: false,
 };
+
+let lastMovie;
+let rate;
+
+a = prompt(`What was the last movie you watched?`, `${lastMovie}`);
+prompt(`Рow much would you rate it?`, `${rate}`);
+
+personalMovieDB.movies[a] = b;
